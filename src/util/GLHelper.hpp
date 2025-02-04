65442f11a643fd90e40e3f726341404ce8c59054
Cameron Pilchard <campilchard@gmail.com>
2025-02-04 12:59:23 -0600

    GL helper function for error handling

---
+++ src/util/GLHelper.hpp
@@ -0,0 +1,25 @@
+
+#ifndef GLHELPER_H
+#define GLHELPER_H
+
+#include <iostream>
+#include <glad/gl.h>
+
+#define glCall(x) GL_clearError(); \
+	x; \
+	GL_logCall(#x, __FILE__, __LINE__);
+
+static void GL_clearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static void GL_logCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError()) {
+		std::cout << "[OpenGL error] " << error << "\n" << file << "\n" << function << "\n" << line << "\n";
+		exit(-1);
+	}
+}
+
+#endif
